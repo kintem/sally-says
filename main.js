813e9eb9727862a28dd165a39a516e00fd6cfa9a
Kinte Matulyte <=>
2021-03-21 11:26:10 +0000

    write random number generator function, flash generator function, add event listeners to each box

---
+++ main.js
@@ -0,0 +1,40 @@
+// 1. sally picks random Number
+// 2. number is added to Array
+// 3. corresponding box flashes
+// 4. add event listeners to all boxes
+// 5. detect user input
+// 6. add user input to user array
+// 7. check user input === sallys input at same index - if true go to 8, if false change heading to game over and add specific styling to page
+// 8. if userinput.length === sallys input.length -  1. increment level 2. eassign user array to empty array??? 3. sally generates new input (call number function again)
+
+const boxes = document.querySelectorAll(".box-container__box");
+const redBox = document.getElementById("1");
+const blueBox = document.getElementById("2");
+const yellowBox = document.getElementById("3");
+const greenBox = document.getElementById("4");
+
+let sallysInput = [];
+let userInput = [];
+let level = 0;
+
+const sallySays = () => {
+  const sallysNewInput = Math.round(Math.random()*4);
+  sallysInput.push(sallysNewInput);
+  generateFlash(sallysNewInput);
+};
+
+const generateFlash = (num) => {
+  document.getElementById(num).classList.add = "flash";
+
+  setTimeout(()=>{
+    document.getElementById(num).classList.remove = "flash";
+  }, 2000)
+};
+
+boxes.forEach(box => {
+  box.addEventListener("click", event => {
+    const userNewInput = event.target.id
+    userInput.push(userNewInput);
+    generateFlash(userNewInput);
+  })
+});
