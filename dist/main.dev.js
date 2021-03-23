b3943f54253eb24fbc868a624a8507b2ff8c32e0
Kinte Matulyte <=>
2021-03-23 21:49:40 +0000

    startGame() function complete with dynamic styling

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -8,8 +8,13 @@
 // 6. add user input to user array
 // 7. check user input === sallys input at specific index - if true go to 8, if false change heading to game over and add specific styling to page
 // 8. if userinput.length === sallys input.length -  1. increment level 2. reassign user array to empty array??? 3. sally generates new input (call number function again)
+var body = document.querySelector("body");
+var start = document.querySelector(".start");
+var game = document.querySelector(".game");
+var startButton = document.querySelector(".start__button");
 var boxes = document.querySelectorAll(".box-container__box");
-var heading = document.querySelector("p");
+var heading = document.querySelector(".game-header__heading");
+var text = document.querySelector(".game-header__text");
 var sallysInputs = [];
 var userInputs = [];
 var level = 0; //generates a "flash" on the chosen box
@@ -40,7 +45,7 @@ var sallySays = function sallySays() {
   var sallysNewInput = Math.ceil(Math.random() * 4);
   sallysInputs.push(sallysNewInput);
   level++;
-  heading.innerHTML = "Level: ".concat(level);
+  text.innerHTML = "Level: ".concat(level);
   generateFlash(sallysNewInput);
 }; // compares users input with sallys input (at same index?)
 // if all correct and array lengths are the same, call sallySays() and reset user array to an empty array, if incorrect - game over
@@ -65,9 +70,26 @@ var gameOver = function gameOver() {
   userInput = [];
   sallysInputs = [];
   level = 0; //change page styling
-}; //turn into function called startGame();
 
+  body.style.backgroundColor = "red";
+  boxes.forEach(function (box) {
+    box.style.backgroundColor = "black";
+  });
+};
 
-document.body.addEventListener("keydown", function () {
-  sallySays();
-});
+var startGame = function startGame() {
+  start.style.display = "none";
+  game.style.display = "contents";
+  body.style.backgroundColor = "black";
+  heading.style.color = "white";
+  text.style.color = "white";
+  setTimeout(function () {
+    sallySays();
+  }, 800);
+};
+
+startButton.addEventListener("click", function () {
+  startGame();
+}); // document.body.addEventListener("keydown", ()=>{
+//     sallySays();
+// });
